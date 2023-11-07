874a7b129f7c775dba0352b8aaee709331d79540
Lorys <116164363+MrStagiaire@users.noreply.github.com>
2023-11-07 05:04:45 +0100

    Update main.py
    Update app V 1.0.1

--- python_test/main.py
+++ python_test/main.py
@@ -2,7 +2,7 @@ import sys
 import random
 from alive_progress import alive_bar
 import time
-# fix le problemme au niveau des message a la fin du choix
+
 class Menu:
     def __init__(self):
         self.is_game_run = True
@@ -16,18 +16,18 @@ class Menu:
         ]
         self.nationalities = [
             "Française",
-            "Américaine",
+            "Americaine",
             "Italienne",
             "Mexicaine",
             "Indienne",
         ]
-        # self.lastmessage = [
-        #     "Bonne app chef !"
-        #     "Bas alors c'est qui qui est content(e) !"
-        #     "Le Stagiaire peux etre en CDI maintenant ouuuu ?"
-        #     "Mama C'est beau mes Morts"
-        #     "Et sa ca fait plaisir... nan...oui bon bas faut savoir merde :( "
-        # ]
+        self.last_messages = [
+            "Bonne app chef !",
+            "Bas alors c'est qui qui est content(e) !",
+            "Le Stagiaire peut-être en CDI maintenant ouuuu ?",
+            "Mama C'est beau mes Morts",
+            "Et ça fait plaisir... nan... oui bon bas faut savoir merde :(",
+        ]
 
     def print_slow(self, str):
         for letter in str:
@@ -47,7 +47,7 @@ class Menu:
 
     def menu(self):
         self.print_slow("\n\t\t[1] Commencer\t\t")
-        self.print_slow("\n\t\t[2] Crédits")
+        self.print_slow("\n\t\t[2] Credits")
         self.print_slow("\n\t\t[4] Quitter\t\t\n\n")
         self.print_slow("DEMO")
         self.print_slow("© by MrStagiaire\n")
@@ -61,6 +61,7 @@ class Menu:
             for i in range(500):
                 time.sleep(0.001)
                 yield
+
         with alive_bar(500) as bar:
             for i in compute():
                 bar()
@@ -69,8 +70,7 @@ class Menu:
             self.print_slow("Vous avez fait une erreur.")
             chose_your_meal = input()
 
-        
-        choix_restaurant = random.choice(self.restaurants) 
+        choix_restaurant = random.choice(self.restaurants)
         
         if choix_restaurant in ["Fast Food", "Japonais", "Chinois", "Vietnamien"]:
             self.print_slow(
@@ -80,38 +80,43 @@ class Menu:
             self.print_slow(
                 f"Nous vous recommandons de manger au restaurant {choix_restaurant} de cuisine {choix_nationalite}.\n")
 
-        self.is_game_run = False
+        random_message = random.choice(self.last_messages)
+        self.print_slow(random_message)
+
+        # Demande de confirmation pour retourner au menu
+        return_menu = input("Voulez-vous retourner au menu principal ? (O/N) ")
+        if return_menu.lower() != "o":
+            self.print_slow("Vous avez quitte On mange quoi.\n")
+            exit()
 
     def credits(self):
-        self.print_slow("\n\t\t[ Crédits ]\n")
+        self.print_slow("\n\t\t[ Credits ]\n")
         self.print_slow("\n\tLe Stagiaire - On mange quoi\n\t")
         self.print_slow("")
 
-    def back(self):
-        return self.menu()
+        # Demande de confirmation pour retourner au menu
+        return_menu = input("Voulez-vous retourner au menu principal ? (O/N) ")
+        if return_menu.lower() != "o":
+            self.print_slow("Vous avez quitte On mange quoi.\n")
+            exit()
 
     def choice_input(self):
-
         while self.is_game_run:
             self.menu()
-            choice = int(input("Que voulez-vous faire ? "))
-            while choice != 1 and choice != 2 and choice != 3 and choice != 4:
-                self.print_slow("Vous avez fait une erreur.\n")
-                self.menu()
-                choice = int(input("Que voulez-vous faire ? "))
-
-            if choice == 1:
+            choice = input("Que voulez-vous faire ? ")
+        
+            if choice == "1":
                 self.play()
-
-            elif choice == 2:
+            elif choice == "2":
                 self.credits()
-
-            elif choice == 4:
-                self.print_slow("Vous avez quitté On mange quoi.\n")
-                exit()
+            elif choice == "4":
+                # Demande de confirmation pour quitter l'application
+                confirm_quit = input("Voulez-vous vraiment quitter On mange quoi ? (O/N) ")
+                if confirm_quit.lower() == "o":
+                    self.print_slow("Vous avez quitte On mange quoi.\n")
+                    exit()
             else:
-                self.print_slow("Sélectionnez parmi les choix proposés !")
-
+                self.print_slow("Selectionnez parmi les choix proposes !")
 
 m = Menu()
 m.print_slow_titre()
